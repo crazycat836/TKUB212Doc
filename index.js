eda03cfe1d6691cb5b8b39e9a4ee55dbdda5a2bf
Gary <crazycat836@gmail.com>
2016-10-25 01:39:00 +0800

    add new css module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('./src/js/init.js');
 require('./src/styles/materialize.css');
+require('./src/styles/app.css');
 require.context(
     "./src/imgs", // context folder
     true, // include subdirectories
