704a2c239bcb2507539d8973be6c10ea24dbe4be
Gary <crazycat836@gmail.com>
2016-11-01 18:24:46 +0800

    require lazyload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('./src/js/init.js');
 require('./src/styles/materialize.css');
 require('./src/styles/app.css');
+require('./src/js/jquery.lazyload.js');
+require('./src/js/jquery.scrollstop.js');
 require.context(
     "./src/imgs", // context folder
     true, // include subdirectories
