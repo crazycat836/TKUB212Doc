842df23cd6e0796fe7d3f1dce665ff1547372b29
Gary <crazycat836@gmail.com>
2017-05-17 12:30:21 +0800

    Change File name

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('./src/js/init.js');
-require('./src/styles/materialize-0.97.8.css');
+require('./src/styles/materialize.css');
 require('./src/styles/app.css');
 require('./src/js/jquery.lazyload.js');
 require('./src/js/jquery.scrollstop.js');
