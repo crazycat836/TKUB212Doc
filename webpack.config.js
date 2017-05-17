74c6e91c95c92df8ffb15251a5c3dc4f79500e7d
Gary <crazycat836@gmail.com>
2017-05-17 12:33:23 +0800

    Update webpack version
    fix #19

--- webpack.config.js
+++ webpack.config.js
@@ -1,93 +1,77 @@
-var webpack = require('webpack');
-var path = require('path');
-var glob = require('glob');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var Purify = require("purifycss-webpack-plugin");
-var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-var debug = process.env.NODE_ENV !== 'production';
+const webpack = require('webpack');
+const path = require('path');
+const glob = require('glob');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+//const PurifyCSSPlugin = require('purifycss-webpack');
+//const debug = process.env.NODE_ENV !== 'production';
 
-var plugins = [
-    new webpack.NoErrorsPlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
+const plugins = [
+    new webpack.NoEmitOnErrorsPlugin(),
     new ExtractTextPlugin('css/[name].css'),
-    new Purify({
+    new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+    }),
+    new UglifyJSPlugin({
+        beautify: false,
+        comments: false,
+        compress: {
+            warnings: false,
+            drop_console: true,
+            collapse_vars: true,
+            reduce_vars: true,
+        }
+    }),
+    /*
+    new PurifyCSSPlugin({
+        //minimize:true,
         purifyOptions: {
-            // Will minify CSS code in addition to purify.
-            minify: true,
-            whitelist: ['pinned', 'pin-top', 'pin-bottom','sidenav-overlay']
+            whitelist: ['pinned', 'pin-top', 'pin-bottom', 'sidenav-overlay','collapsible.','table-of-contents,']
         },
-
-        basePath: path.join(__dirname, 'src'),
-        paths: [
-            "views/*.html"
-        ]
-    })
-
+        paths: glob.sync(path.join(__dirname, 'src/views/*.html'))
+    })*/
 ];
 
-if (debug) {
-    //DEVELOPMENT
-    plugins.push(
-        new webpack.HotModuleReplacementPlugin({
-            multiStep: true
-        })
-    );
-} else {
-    // PRODUCTION
-    plugins.push(
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            dropDebugger: true,
-            dropConsole: true,
-            compress: {
-                warnings: false,
-            },
-            output: {
-                comments: false,
-            }
-        })
-    );
-}
-var config = module.exports = {
-
+const config = module.exports = {
     cache: true,
-    debug: debug,
-    //devtool: debug ? 'eval' : 'source-map',
     entry: {
         main: './index.js'
     },
     output: {
         filename: 'js/[name].js',
-        path: path.join(__dirname, 'build'),
+        path: path.join(__dirname, 'dist'),
         publicPath: './'
     },
     module: {
-        loaders: [{
+        rules: [{
             test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style', 'css')
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+            })
         }, {
             test: /\.html$/,
-            loader: "html?-minimize" //避免壓縮HTML
+            use: "html-loader?-minimize" //避免壓縮HTML
         }, {
             test: /\.(svg|png|gif|jpe?g|JPE?G)$/,
-            loader: 'url?limit=1&name=imgs/[name].[ext]'
+            use: 'url-loader?limit=1&name=imgs/[name].[ext]'
         }]
     },
     resolve: {
-        root: [path.resolve('./src')],
+        modules: [
+            path.join(__dirname, "src")
+        ]
     },
     plugins: plugins
 };
 
-
-var pages = Object.keys(getEntry('src/views/*.html'));
+const pages = Object.keys(getEntry('src/views/*.html'));
 pages.forEach(function(pathname) {
-    var conf = {
+    const conf = {
         filename: './' + pathname + '.html', //html output dest
         template: 'src/views/' + pathname + '.html', //html template
-
-
     };
     if (pathname in config.entry) {
         //conf.favicon = 'src/imgs/favicon.ico';
@@ -99,11 +83,11 @@ pages.forEach(function(pathname) {
 });
 
 function getEntry(globPath, pathDir) {
-    var files = glob.sync(globPath);
-    var entries = {},
+    const files = glob.sync(globPath);
+    let entries = {},
         entry, dirname, basename, pathname, extname;
 
-    for (var i = 0; i < files.length; i++) {
+    for (let i = 0; i < files.length; i++) {
         entry = files[i];
         dirname = path.dirname(entry);
         extname = path.extname(entry);
