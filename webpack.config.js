764e6d883bd1a548eb8a712949ec3976a7eed88a
crazycat836 <crazycat836@gmail.com>
2018-02-27 23:49:54 +0800

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ const plugins = [
             }
         }
     }),
+
     new PurifyCSSPlugin({
         // Give paths to parse for rules. These should be absolute!
         paths: glob.sync(path.join(__dirname, 'src/views/*.html')),
