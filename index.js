507f589855f2883130b1d1570bb6b548dda12cd8
Gary Shih <crazycat836@gmail.com>
2018-09-30 22:43:23 +0800

    delete unuse js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('./src/js/init.js');
 require('./src/styles/materialize.css');
 require('./src/styles/app.css');
 require('./src/js/jquery.lazyload.js');
-require('./src/js/jquery.scrollstop.js');
 require.context(
     "./src/imgs", // context folder
     true, // include subdirectories
