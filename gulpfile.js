85ed493588811479c257839c5dd96b94a128c72f
Gary Shih <crazycat836@gmail.com>
2018-10-01 17:21:05 +0800

    fix #24
    升級到 gulp 4 採用新的 gulp.series API

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,19 @@
-var gulp = require('gulp'),
-    pug = require('gulp-pug'),
-    clean = require('gulp-clean'),
-    runSequence = require('run-sequence');
-
-
-gulp.task('default', function(callback) {
-    runSequence('clean',
-        'pug',
-        callback);
-});
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const cleanup = require('gulp-clean');
 
 // turn jade into html
-gulp.task('pug', function() {
+gulp.task('pug', function () {
     return gulp.src('src/pugs/*.pug')
-        .pipe(pug({ pretty: true })) // pip to pug plugin
+        .pipe(pug({pretty: true})) // pip to pug plugin
         .pipe(gulp.dest('src/views')); // tell gulp our output folder
 });
 
+
 // Clean Task
-gulp.task('clean', function() {
-    return gulp.src(['dist/*', 'src/views/*'], { read: false })
-        .pipe(clean());
+gulp.task('clean', function () {
+    return gulp.src(['dist/*', 'src/views/*'], {read: false})
+        .pipe(cleanup());
 });
+
+gulp.task('default', gulp.series('clean', 'pug'));
